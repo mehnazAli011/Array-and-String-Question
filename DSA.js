d24dd32af2be7e895d16d1e50955fcb462a57d6d
unknown <mehnazm406@gmail.com>
2024-09-19 16:30:20 +0530

    DSA Questions

--- DSA.js
+++ DSA.js
@@ -3640,3 +3640,70 @@ function convertTemp(celsius) {
 }
 console.log(convertTemp(36.5));
 console.log(convertTemp(122.11));
+// ======================================================================================
+// Q 80 Final the value after performing Operations
+// There is a programming language with only four operations and one variable X:
+
+// ++X and X++ increments the value of the variable X by 1.
+// --X and X-- decrements the value of the variable X by 1.
+// Initially, the value of X is 0.
+
+// Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+// Example 1:
+
+// Input: operations = ["--X","X++","X++"]
+// Output: 1
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// --X: X is decremented by 1, X =  0 - 1 = -1.
+// X++: X is incremented by 1, X = -1 + 1 =  0.
+// X++: X is incremented by 1, X =  0 + 1 =  1.
+// Example 2:
+
+// Input: operations = ["++X","++X","X++"]
+// Output: 3
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// ++X: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// X++: X is incremented by 1, X = 2 + 1 = 3.
+// Example 3:
+
+// Input: operations = ["X++","++X","--X","X--"]
+// Output: 0
+// Explanation: The operations are performed as follows:
+// Initially, X = 0.
+// X++: X is incremented by 1, X = 0 + 1 = 1.
+// ++X: X is incremented by 1, X = 1 + 1 = 2.
+// --X: X is decremented by 1, X = 2 - 1 = 1.
+// X--: X is decremented by 1, X = 1 - 1 = 0.
+
+//Better Approach
+// function finalValueAfterOperations(operations) {
+//   let x = 0;
+//   for (let i = 0; i < operations.length; i++) {
+//     if (operations[i] === "X++" || operations[i] === "++X") {
+//       x++;
+//     } else {
+//       x--;
+//     }
+//   }
+//   return x;
+// }
+// console.log(finalValueAfterOperations(["--X", "X++", "X++"]));
+// console.log(finalValueAfterOperations(["++X", "++X", "X++"]));
+// console.log(finalValueAfterOperations(["X++", "++X", "--X", "X--"]));
+
+//Better Approach
+function finalValueAfterOperations(operations) {
+  let x = 0;
+  for (let i = 0; i < operations.length; i++) {
+    x += operations[i][1] === "+" ? 1 : -1;
+  }
+  return x;
+}
+console.log(finalValueAfterOperations(["--X", "X++", "X++"]));
+console.log(finalValueAfterOperations(["++X", "++X", "X++"]));
+console.log(finalValueAfterOperations(["X++", "++X", "--X", "X--"]));
+// =====================================================================================
